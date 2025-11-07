ad07f7eddebd3460200a556d27e7da0b09702f4c
CalurianTech <caluriantech@gmail.com>
2025-11-07 02:23:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect('https://' + req.headers.host + req.url);
+  }
+  next();
+});
